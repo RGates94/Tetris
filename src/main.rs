6af0efe3aba24aec852710ece20073591a40e956
RGates94 <rgates275@gmail.com>
2020-05-16 14:02:53 -0400

    Divide logic into functions for reusability

--- src/main.rs
+++ src/main.rs
@@ -88,6 +88,18 @@ struct Board {
 }
 
 impl Board {
+    fn check_collision(&self, piece: Piece, column: u8, row: u8, rotation: u8) -> bool {
+        let mut collides = false;
+        for (x, y) in piece.filled(rotation) {
+            collides |= match self.board.get((row + *x) as usize).map(|x|x[(column + *y) as usize]
+                .filled
+                .is_some()) {
+                Some(val) => val,
+                None => continue,
+            }
+        }
+        collides
+    }
     fn hard_drop(&mut self, piece: Piece, column: u8, rotation: u8) {
         if column >= 10 {
             return;
@@ -95,18 +107,9 @@ impl Board {
         let mut target_row = 0;
         for row in (0..20).rev() {
             if {
-                let mut collides = false;
-                for (x, y) in piece.filled(rotation) {
-                    collides |= match self.board.get(row + *x as usize).map(|x|x[(column + *y) as usize]
-                        .filled
-                        .is_some()) {
-                        Some(val) => val,
-                        None => continue,
-                    }
-                }
-                collides
+                self.check_collision(piece,column,row,rotation)
             } {
-                if row < 20 - piece.height(rotation) {
+                if (row as usize) < 20 - piece.height(rotation) {
                     target_row = row + 1;
                     break;
                 } else {
@@ -115,9 +118,41 @@ impl Board {
                 }
             }
         }
-        for (x, y) in piece.filled(rotation) {
-            self.board[target_row + *x as usize][(column + *y) as usize].filled = Some(piece)
+        self.place_unchecked(piece, column, target_row, rotation);
+    }
+    fn _place_checked(&mut self, piece: Piece, column: u8, row: u8, rotation: u8) -> bool {
+        if self.check_collision(piece,column,row,rotation) {
+            self.place_unchecked(piece,column,row,rotation);
+            true
+        } else {
+            false
         }
+    }
+    fn place_unchecked(&mut self, piece: Piece, column: u8, row: u8, rotation: u8) {
+        for (x, y) in piece.filled(rotation) {
+            self.board[(row + *x) as usize][(column + *y) as usize].filled = Some(piece)
+        }
+    }
+    fn draw_board_ggez(&self, ctx: &mut Context, x: f32, y: f32) -> GameResult {
+        for (ypos, row) in self.board.iter().enumerate() {
+            for (xpos, cell) in row.iter().enumerate() {
+                if cell.filled.is_some() {
+                    let rectangle = graphics::Mesh::new_rectangle(
+                        ctx,
+                        graphics::DrawMode::fill(),
+                        Rect::new(
+                            x + 1.0 + 16.0 * xpos as f32,
+                            y + 1.0 - 16.0 * (ypos + 1) as f32,
+                            14.0,
+                            14.0,
+                        ),
+                        cell.filled.unwrap().color(),
+                    )?;
+                    graphics::draw(ctx, &rectangle, (ggez::mint::Point2 { x: 0.0, y: 0.0 },))?;
+                }
+            }
+        }
+        Ok(())
     }
 }
 
@@ -136,23 +171,33 @@ struct Tetris {
     next_batch: Vec<Piece>,
 }
 
+impl Tetris {
+    fn next_piece(&mut self) -> Piece {
+        if self.current_batch.is_empty() {
+            swap(&mut self.current_batch, &mut self.next_batch);
+            generate_batch(&mut self.rng, &mut self.next_batch);
+            if self.current_batch.is_empty() {
+                generate_batch(&mut self.rng, &mut self.current_batch)
+            }
+        }
+        self.current_batch.pop().unwrap()
+    }
+    fn place_random(&mut self) {
+        let piece = self.next_piece();
+        let rotation = self.rng.gen_range(0, 4);
+        let column = self.rng.gen_range(0, 11 - piece.width(rotation) as u8);
+        self.board.hard_drop(piece, column, rotation);
+    }
+}
+
 impl EventHandler for Tetris {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        if let Some(time) = &mut self.next_tick {
-            while Instant::now() > *time {
-                if self.current_batch.is_empty() {
-                    swap(&mut self.current_batch, &mut self.next_batch);
-                    generate_batch(&mut self.rng, &mut self.next_batch);
-                    if self.current_batch.is_empty() {
-                        generate_batch(&mut self.rng, &mut self.current_batch)
-                    }
-                }
-                let piece = self.current_batch.pop().unwrap();
-                let rotation = self.rng.gen_range(0, 4);
-                let column = self.rng.gen_range(0, 11 - piece.width(rotation) as u8);
-                self.board.hard_drop(piece, column, rotation);
-                *time += self.tick_speed;
+        if let Some(mut time) = self.next_tick {
+            while Instant::now() > time {
+                self.place_random();
+                time += self.tick_speed;
             }
+            self.next_tick = Some(time);
         }
         Ok(())
     }
@@ -188,24 +233,7 @@ impl EventHandler for Tetris {
         graphics::draw(ctx, &outer, (ggez::mint::Point2 { x: 0.0, y: 0.0 },))?;
         graphics::draw(ctx, &inner, (ggez::mint::Point2 { x: 0.0, y: 0.0 },))?;
 
-        for (ypos, row) in self.board.board.iter().enumerate() {
-            for (xpos, cell) in row.iter().enumerate() {
-                if cell.filled.is_some() {
-                    let rectangle = graphics::Mesh::new_rectangle(
-                        ctx,
-                        graphics::DrawMode::fill(),
-                        Rect::new(
-                            width as f32 / 2.0 - 79.0 + 16.0 * xpos as f32,
-                            height as f32 + 1.0 - 16.0 * (ypos + 1) as f32,
-                            14.0,
-                            14.0,
-                        ),
-                        cell.filled.unwrap().color(),
-                    )?;
-                    graphics::draw(ctx, &rectangle, (ggez::mint::Point2 { x: 0.0, y: 0.0 },))?;
-                }
-            }
-        }
+        self.board.draw_board_ggez(ctx, width as f32 / 2.0 - 80.0, height as f32)?;
 
         graphics::present(ctx)
     }
